c69be8b68d50643c1394aa57445c52d02906580b
Jefferson Goncalves <jeffgoncalves321@gmail.com>
2026-09-21 16:17:53 -0300

    findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,18 +6,20 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     void main() {
-        Department obj = new Department(1, "Books");
-
-        Seller seller1 = new Seller(21, "Bob", "bob@@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("====== TEST 1: SELLER findById ======");
         Seller seller2 = sellerDao.findByID(3);
 
-        System.out.println(seller2);
+        System.out.println("\n====== TESTE 2: seller findByDepartment ======");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj2 : list) {
+            System.out.println(obj2);
+        }
     }
 }
