cd3e4dcf9005af83d4b1c850410c8e58f9166879
Jefferson Goncalves <jeffgoncalves321@gmail.com>
2026-09-21 17:15:42 -0300

    departmentDao implemented

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -2,9 +2,14 @@ package model.dao;
 
 import db.DB;
 import model.impl.SellerDaoJDBC;
+import model.impl.DepartmentDaoJDBC;
 
 public class DaoFactory {
     public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
